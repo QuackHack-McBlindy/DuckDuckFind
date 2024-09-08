ffd9bd9abf587a35144c01fbe5f1084e5270c8dc
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2024-09-08 07:22:31 +0200

    Create allowed_regions.py

---
+++ app/settings/allowed_regions.py
@@ -0,0 +1,12 @@
+# List of allowed regions for regional web searches
+ALLOWED_REGIONS = [
+    "xa-ar", "xa-en", "ar-es", "au-en", "at-de", "be-fr", "be-nl", "br-pt", 
+    "bg-bg", "ca-en", "ca-fr", "ct-ca", "cl-es", "cn-zh", "co-es", "hr-hr", 
+    "cz-cs", "dk-da", "ee-et", "fi-fi", "fr-fr", "de-de", "gr-el", "hk-tzh", 
+    "hu-hu", "in-en", "id-id", "id-en", "ie-en", "il-he", "it-it", "jp-jp", 
+    "kr-kr", "lv-lv", "lt-lt", "xl-es", "my-ms", "my-en", "mx-es", "nl-nl", 
+    "nz-en", "no-no", "pe-es", "ph-en", "ph-tl", "pl-pl", "pt-pt", "ro-ro", 
+    "ru-ru", "sg-en", "sk-sk", "sl-sl", "za-en", "es-es", "se-sv", "ch-de", 
+    "ch-fr", "ch-it", "tw-tzh", "th-th", "tr-tr", "ua-uk", "uk-en", "us-en", 
+    "ue-es", "ve-es", "vn-vi", "wt-wt"
+]
